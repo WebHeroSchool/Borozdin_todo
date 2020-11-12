3dde7060135bd7ebb08ebbeecf0f7814d7f254a5
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-12 19:43:32 +0700

    Добавлены оставшиеся примеры

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const word = 'Hello world!';
-const count = 20;
+const count = 7;
+const flag = true;
 
 function App() {
   return (
@@ -16,10 +17,20 @@ function App() {
           {word}
         </p>
         <p style={{margin:0}}>
-          {count}
+          Count = {count}
         </p>
         <p style={{margin:0}}>
            1 + 4 = {1 + 4}
+        </p>
+        <p style={{margin:0}}>
+           {flag && 'Flag is true'}
+        </p>
+        <p style={{margin:0}}>
+           {(count>=(1+4)) ? 'Count >= 1+4' : 'Count < 1+4'}
+           {undefined}
+           {null}
+           {false}
+           {true}
         </p>
         <a
           className="App-link"
