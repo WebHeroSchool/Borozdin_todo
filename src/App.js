6c0d5980a1553b76e174daf76464fc77eef91082
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-11-13 13:22:12 +0700

    Созданы react компоненты

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-const word = 'Hello world!';
-const count = 7;
-const flag = true;
+const ItemList = () => (<ul>
+  <li>1</li>
+  <li>2</li>
+</ul>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{color:'blue', margin:0}}>
-          {word}
-        </p>
-        <p style={{margin:0}}>
-          Count = {count}
-        </p>
-        <p style={{margin:0}}>
-           1 + 4 = {1 + 4}
-        </p>
-        <p style={{margin:0}}>
-           {flag && 'Flag is true'}
-        </p>
-        <p style={{margin:0}}>
-           {(count>=(1+4)) ? 'Count >= 1+4' : 'Count < 1+4'}
-           {undefined}
-           {null}
-           {false}
-           {true}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (<div>
+    <h1>Привет друг!</h1>
+    <p>Список дел:</p>
+    <ItemList />
+  </div>);
 
-export default App;
+  export {App};
