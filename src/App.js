aaf46a461dafe0982352c7674ff9f4c6f8666103
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-11-13 11:45:44 +0700

    Удалены лишние файлы, добавлен элемент в app.js  и передан в ReactDOM.render() в index.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const element = (<div>
+    <h1>Hello friend!</h1>
+    <p>Have a nice day</p>
+  </div>);
 
-const word = 'Hello world!';
-const count = 7;
-const flag = true;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{color:'blue', margin:0}}>
-          {word}
-        </p>
-        <p style={{margin:0}}>
-          Count = {count}
-        </p>
-        <p style={{margin:0}}>
-           1 + 4 = {1 + 4}
-        </p>
-        <p style={{margin:0}}>
-           {flag && 'Flag is true'}
-        </p>
-        <p style={{margin:0}}>
-           {(count>=(1+4)) ? 'Count >= 1+4' : 'Count < 1+4'}
-           {undefined}
-           {null}
-           {false}
-           {true}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  export {element};
