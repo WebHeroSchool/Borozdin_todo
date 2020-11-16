9164de68f61344649d2ff8d655b409d8006e681d
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-16 18:54:27 +0700

    В footer с помощью props передаётся колличество дел. Добавлен вывод в Item  3 разных дел

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,15 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
+const todoItem1 = 'Сделать завтрак';
+const todoItem2 = 'Сделать тренировку';
+const todoItem3 = 'Сходить в магазин';
+
 const App = () => (<div>
     <h1>todos</h1>
     <InputItem />
-    <ItemList />
-    <Footer />
+    <ItemList todoItem1={todoItem1} todoItem2={todoItem2} todoItem3={todoItem3} />
+    <Footer count={3} />
   </div>);
 
 export default App;
