c1daa07929ea9b06aa313ae4b2b504941d58fb4f
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-18 19:04:51 +0700

    Добавлен компонент Material UI

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>What needs to be done?</div>);
+const InputItem = () => (<div>
+  <TextField id="standard-basic" label="Добавить задание" />
+</div>);
 
 export default InputItem;
