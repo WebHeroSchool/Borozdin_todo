078bcdbfcde1b38f4aa9554f3f03fb029287757d
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-11-20 13:14:28 +0700

    Передлелан App из компонента-функции в компонент-класс

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,34 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Сделать завтрак',
-      isDone: true
-    },
-    {
-      value: 'Сделать тренировку',
-      isDone: false
-    },
-    {
-      value: 'Сходить в магазин',
-      isDone: false
-    }
-  ];
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Сделать завтрак',
+        isDone: true
+      },
+      {
+        value: 'Сделать тренировку',
+        isDone: false
+      },
+      {
+        value: 'Сходить в магазин',
+        isDone: false
+      }
+    ];
 
-  return (
-    <div className = {styles.wrap}>
-      <h1 className = {styles.title}>todos</h1>
-      <div className = {styles.todoBody}>
-        <InputItem />
-        <ItemList items={items} />
-        <Footer count={items.length} />
+    return (
+      <div className = {styles.wrap}>
+        <h1 className = {styles.title}>todos</h1>
+        <div className = {styles.todoBody}>
+          <InputItem />
+          <ItemList items={items} />
+          <Footer count={items.length} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
