5c1e4f53156614fcd247a24dc7570a137f1316e3
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-20 21:01:43 +0700

    Добавлено изменение состояния по клику на сам пункт

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,29 +5,48 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const items = [
+  state = {
+    items : [
       {
         value: 'Сделать завтрак',
-        isDone: true
+        isDone: true,
+        id: 1
       },
       {
         value: 'Сделать тренировку',
-        isDone: false
+        isDone: false,
+        id: 2
       },
       {
         value: 'Сходить в магазин',
-        isDone: false
+        isDone: false,
+        id: 3
       }
-    ];
+    ]
+  };
 
+  onClickDone = id => {
+    const newItemList = this.state.items.map (item=> {
+      const newItem = { ...item };
+
+      if(item.id === id) {
+        newItem.isDone = !item.isDone;
+      }
+
+      return newItem;
+    });
+
+    this.setState({ items: newItemList})
+  };
+
+  render() {
     return (
       <div className = {styles.wrap}>
         <h1 className = {styles.title}>todos</h1>
         <div className = {styles.todoBody}>
           <InputItem />
-          <ItemList items={items} />
-          <Footer count={items.length} />
+          <ItemList items={this.state.items} onClickDone={this.onClickDone} />
+          <Footer count={this.state.items.length} />
         </div>
       </div>
     );
