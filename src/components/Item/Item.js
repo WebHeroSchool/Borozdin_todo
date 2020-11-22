9301d4db377dcd2bb8f7d53bad390002fb58b833
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-22 20:14:01 +0700

    Добавлены иконки корзины

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,6 +3,9 @@ import classnames from 'classnames';
 import styles from './Item.module.css';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 
 const Item =({ value, isDone, onClickDone, id }) => (<span className={
   classnames({
@@ -12,6 +15,11 @@ const Item =({ value, isDone, onClickDone, id }) => (<span className={
 }>
   <ListItem button onClick={() => onClickDone(id)} >
     <ListItemText primary={ value } />
+    <ListItemSecondaryAction>
+      <IconButton edge="end" aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
   </ListItem>
 </span>);
 
