a8d57296d4f45d1c031f33b70a44788d02baaa72
Бороздин Дмитрий <borozdin1@yandex.ru>
2020-11-26 18:05:53 +0700

    Заполнены компоненты "Обо мне" и "Контакты".

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+
+const About = () => {
+  return (
+    <CardContent style={{minWidth: 550}}>
+      <h1>
+        About
+      </h1>
+    </CardContent>
+  )
+}
+
+export default About;
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardContent from '@material-ui/core/CardContent';
+
+const Contacts = () => {
+  return (
+    <CardContent style={{minWidth: 550}}>
+      <h1>
+        Contacts
+      </h1>
+    </CardContent>
+  )
+}
+
+export default Contacts;
