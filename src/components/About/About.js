7543e4c1fda554882ff40a9dd1c62c85babe5e8e
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-12-12 15:35:27 +0700

    Добавлена информация о юзере: вывод картинки, имени, био, ссылка на почту и телеграм

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
+import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -10,6 +9,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Link from '@material-ui/core/Link';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import {Octokit} from '@octokit/rest';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
+import TelegramIcon from '@material-ui/icons/Telegram';
 import styles from './About.module.css';
 
 const octokit = new Octokit();
@@ -20,16 +21,13 @@ class About extends React.Component {
     repoList: [],
     fetchFailure: false,
     failure: '',
-    user: 'DmBorozdin',
-    userName:'',
-    userAvatarUrl: '',
-    userBio:'',
-    userUrl:''
+    userName: 'DmBorozdin',
+    User: []
   }
 
   componentDidMount() {
     octokit.repos.listForUser ({
-      username: this.state.user
+      username: this.state.userName
     }).then (({data}) => {
       this.setState({
         repoList: data,
@@ -47,37 +45,46 @@ class About extends React.Component {
     });
 
     octokit.users.getByUsername ({
-      username: this.state.user
+      username: this.state.userName
     }).then (({data}) => {
       this.setState({
-        userName: data.name,
-        userAvatarUrl: data.avatar_url,
-        userBio: data.bio,
-        userUrl: data.html_url
+        User: data
       })
       console.log(data);
     }).catch((err) => {console.log(err)});
   }
 
   render() {
-    const { isLoading, repoList, fetchFailure, failure, userName, userAvatarUrl, userBio,  userUrl} = this.state;
+    const { isLoading, repoList, fetchFailure, failure, User} = this.state;
 
     return (
       <div>
         { isLoading ? <Card><ProgressBar /></Card> : fetchFailure ?  <Card><Typography variant="h5" component="h2">{failure}</Typography></Card> :
-          <div>
+          <div className={styles.wrap}>
             <Card className={styles.userCard}>
-              <CardHeader
-                avatar = {
-                  <Avatar alt="Аватарка" src={userAvatarUrl}/>
-                }
-                title = {
-                  <Link href={userUrl} color="inherit">
-                    {userName}
-                  </Link>
-                }
-                subheader={userBio}
-              />
+              <CardMedia
+                className={styles.userAvatar}
+                image={User.avatar_url}
+                title="User Avatar"
+                alt="Аватарка"
+              >
+              </CardMedia>
+              <CardContent className={styles.userInfo}>
+                <Typography variant="h6" component="h2" className={styles.userInfoTitle}>
+                  {User.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p" className={styles.userBio}>
+                  {User.bio}
+                </Typography>
+                <Link href="mailto: borozdin1@yandex.ru" color="textSecondary" className={styles.emailLink} underline="none">
+                  <AlternateEmailIcon fontSize="small" className={styles.emailImage}/>
+                  borozdin1@yandex.ru
+                </Link>
+                <Link href="tg://resolve?domain=DmBorozdin" color="textSecondary" className={styles.telegramlLink} underline="none">
+                  <TelegramIcon fontSize="small" className={styles.telegramImage}/>
+                  +7 923 228 77 91
+                </Link>
+              </CardContent>
             </Card>
             <Card className={styles.repoCard}>
               <CardContent>
