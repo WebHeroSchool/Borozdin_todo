0ed07972d447222de809435f7f76054693bc5bcb
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-12-16 21:24:55 +0700

    Доделано оформление вкладки Обо мне

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,17 +5,14 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Link from '@material-ui/core/Link';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import {Octokit} from '@octokit/rest';
 import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import TelegramIcon from '@material-ui/icons/Telegram';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import IconButton from '@material-ui/core/IconButton';
 import styles from './About.module.css';
+
+import errorImg from '../../img/error.svg';
 
 const octokit = new Octokit();
 
@@ -75,11 +72,19 @@ class About extends React.Component {
   };
 
   render() {
-    const { isLoading, repoList, fetchFailure, failure, User, firstRepo, lastRepo} = this.state;
+    const { isLoading, repoList, fetchFailure, User, firstRepo, lastRepo} = this.state;
 
     return (
       <div>
-        { isLoading ? <Card><ProgressBar /></Card> : fetchFailure ?  <Card><Typography variant="h5" component="h2">{failure}</Typography></Card> :
+        { isLoading ? <Card><ProgressBar /></Card> : fetchFailure ?
+          <Card className={styles.repoCard + ' ' + styles.errorCard}>
+            <div className={styles.errorWrap}>
+              <img src={errorImg} alt='Ошибка загрузки' className={styles.errorImg}></img>
+              <h2 className={styles.errorTitle}>Что-то пошло не так...</h2>
+              <p className={styles.errorText}>Попробуйте <a href='.' className={styles.errorRefresh}>загрузить</a> ещё раз</p>
+            </div>
+          </Card> :
+
           <div className={styles.wrap}>
             <Card className={styles.userCard}>
               <CardMedia
@@ -122,7 +127,7 @@ class About extends React.Component {
                 <Typography variant="h6" component="h2">
                   Репозитории на GitHub.com:
                 </Typography>
-                <ul className={styles.repoList}>
+                {repoList.length>0 && <ul className={styles.repoList}>
                   {repoList.slice(firstRepo,lastRepo).map(repo => (
                     <Link href={repo.html_url} className={styles.repolink} color="inherit" key={repo.id} underline="none">
                       <li className={styles.repository}>
@@ -144,12 +149,20 @@ class About extends React.Component {
                         </div>
                       </li>
                     </Link>))}
-                </ul>
+                </ul>}
+                {repoList.length===0 && <div className={styles.repoNone}>
+                  <img src={errorImg} alt='Нет репозиториев' className={styles.repoNoneImg}></img>
+                  <h2 className={styles.repoNoneTitle}>Репозитории отсутствуют</h2>
+                  <p className={styles.repoNoneText}>Добавьте как минимум один репозиторий на <a href='https://github.com' className={styles.repoNoneLink}>github.com</a></p>
+                </div>
+                }
               </CardContent>
-              <CardActions className={styles.buttonWrap}>
-                <Button className={styles.button} variant="outlined" color="primary" disabled={firstRepo ===0} onClick={() => this.onClickBack()}>Назад</Button>
-                <Button className={styles.button} variant="outlined" color="primary" disabled={repoList.length - lastRepo <= 0} onClick={() => this.onClickNext()}>Далее</Button>
-              </CardActions>
+
+              { repoList.length>5 && <CardActions className={styles.buttonWrap}>
+                  <Button className={styles.button} variant="outlined" color="primary" disabled={firstRepo ===0} onClick={() => this.onClickBack()}>Назад</Button>
+                  <Button className={styles.button} variant="outlined" color="primary" disabled={repoList.length - lastRepo <= 0} onClick={() => this.onClickNext()}>Далее</Button>
+                </CardActions>
+              }
             </Card>
           </div>
         }
