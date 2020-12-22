020b64bf8235cbd013e227ef0f5c046946dfdc19
DMITRII-PC\Дмитрий <borozdin1@yandex.ru>
2020-12-22 18:34:16 +0700

    Начата работа над фильтрацией списка

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -7,6 +7,7 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './Todo.module.css';
+import classnames from 'classnames';
 
 import errorImg from '../../img/error.svg';
 
@@ -29,17 +30,25 @@ const Todo = () => {
         id: 3
       }
     ],
+    itemsFilter: [],
+    filter: 'all',
     count:3,
     lastID:3,
     countActive:2,
     countDone:1
   };
 
+
+
   const [items, setItems] = useState (initialState.items);
+  const [itemsFilter, setItemsFilter] = useState (initialState.itemsFilter);
+  const [filter, setFilter] = useState (initialState.filter);
   const [count, setCount] = useState (initialState.count);
   const [lastID, setLastId] = useState (initialState.lastID);
   const [countActive, setCountActive] = useState (initialState.countActive);
   const [countDone, setCountDone] = useState (initialState.countDone);
+
+
 
   useEffect(() => {
     console.log("update");
@@ -47,15 +56,21 @@ const Todo = () => {
     console.log("count:" + count);
     console.log("lastID:" + lastID);
     console.log("countDone:" + countDone);
+    console.log("itemsFilter:" + itemsFilter);
   });
 
   useEffect(() => {
     console.log("mount");
+     setItemsFilter(items);
   }, []);
 
   useEffect(() => {
     console.log("count change");
   }, [count]);
+
+  useEffect(() => {
+    onClickFilter(filter);
+  }, [items]);
 
   const onClickDone = id => {
     const newItemList = items.map (item=> {
@@ -101,32 +116,53 @@ const Todo = () => {
     setCountActive((countActive) => countActive + 1);
   };
 
+  const onClickFilter = filter => {
+    let newItemsList=[];
+    switch (filter) {
+      case 'finished':
+        newItemsList = items.filter (item => item.isDone);
+        break;
+      case 'active':
+        newItemsList = items.filter (item => !item.isDone);
+        break;
+      case 'all':
+        newItemsList = items;
+        break;
+      default:
+        newItemsList = items;
+    }
+    setItemsFilter(newItemsList);
+    setFilter(filter);
+  };
+
   return (
     <Card className={styles.todoCard} style={{borderRadius: 9}}>
       <div className={styles.todoCardHeader}>
         <h3 className = {styles.title}>Список моих дел</h3>
-        <div className = {styles.todoCount}>
-          <div className={styles.completedAffair}>
-            <p className={styles.completedAffairText}>Завершенные</p>
-            <div className={styles.completedAffairNuber}>&nbsp; {countDone} </div>
-          </div>
-          <div className={styles.completedAffair}>
-            <p className={styles.completedAffairText}>Незавершенные </p>
-            <div className={styles.completedAffairNuber}>&nbsp; {countActive}</div>
-          </div>
-          <p className={styles.completedAffairText}>Все</p>
+        <div className = {styles.todoCountWrap}>
+          <button className={classnames({[styles.todoCount]:true, [styles.todoCountActive]: filter==='finished'})} onClick={()=> onClickFilter('finished')}>
+            Завершенные
+            <span className={classnames({[styles.todoCountNuber]:true, [styles.todoCountNuberActive]: filter==='finished'})}>&nbsp; {countDone} </span>
+          </button>
+          <button className={classnames({[styles.todoCount]:true, [styles.todoCountActive]: filter==='active'})} onClick={()=> onClickFilter('active')}>
+            Незавершенные
+            <span className={classnames({[styles.todoCountNuber]:true, [styles.todoCountNuberActive]: filter==='active'})}>&nbsp; {countActive}</span>
+          </button>
+          <button className={classnames({[styles.todoCount]:true, [styles.todoCountActive]: filter==='all'})} onClick={()=> onClickFilter('all')}>Все</button>
         </div>
       </div>
       <CardContent className={styles.todoCardBody}>
-        {items.length>0 ? <ItemList
-          items={items}
-          onClickDone={onClickDone}
-          onClickDelete={onClickDelete}
-        /> : <div className={styles.emptyListWrap}>
-          <img src={errorImg} alt='Ошибка загрузки' className={styles.emptyListImg}></img>
-          <h2 className={styles.emptyListTitle}>Вы ещё не добавили ни одной задачи</h2>
-          <p className={styles.emptyListText}>Сделайте это прямо сейчас!</p>
-        </div>}
+        {items.length>0 ?
+          <ItemList
+            items={itemsFilter}
+            onClickDone={onClickDone}
+            onClickDelete={onClickDelete}
+          /> :
+          <div className={styles.emptyListWrap}>
+            <img src={errorImg} alt='Ошибка загрузки' className={styles.emptyListImg}></img>
+            <h2 className={styles.emptyListTitle}>Вы ещё не добавили ни одной задачи</h2>
+            <p className={styles.emptyListText}>Сделайте это прямо сейчас!</p>
+          </div>}
         <InputItem onClickAdd={onClickAdd} items={items}/>
       </CardContent>
     </Card>
