3a391b673f1d3f00d292eb29bea42d8444789e85
Бороздин Дмитрий <borozdin1@yandex.ru>
2021-03-02 21:56:08 +0700

    Добавлены тесты InputItem

--- src/components/InputItem/InputItem.test.js
+++ src/components/InputItem/InputItem.test.js
@@ -21,7 +21,7 @@ describe('InputItem test',()=>{
       expect(wrapper.find(TextField).prop('helperText')).toEqual('');
     });
     test('В TextField поле ввода пустое', ()=> {
-      expect(wrapper.find(TextField).text()).toEqual('');
+      expect(wrapper.find(TextField).prop('value')).toEqual('');
     });
     test('Создан компонент Fab', ()=> {
       expect(wrapper.find(Fab).exists()).toBe(true);
@@ -39,6 +39,23 @@ describe('InputItem test',()=>{
     });
     test('У TextField выводится сообщение в helperText ="Заполните поле"', ()=> {
       expect(wrapper.find(TextField).prop('helperText')).toEqual('Заполните поле');
+    });
+  });
+
+  describe('Имитируем ввод текста',()=>{
+    const items = [
+      {value:"Изучить React", isDone:false, id:1},
+      {value:"Найти работу",isDone:false, id:2}
+    ];
+    const simInputValue="Написать резюме";
+    const onClickAdd=jest.fn();
+    const wrapper = shallow(<InputItem onClickAdd={onClickAdd} items={items}/>);
+    test('Имитируем ввод текста в поле ввода и проверяем вызов метода onClickAdd', ()=> {
+      wrapper.find(TextField).simulate('change',{target:{value: simInputValue}});
+      expect(wrapper.state('inputValue')).toEqual(simInputValue);
+      expect(wrapper.find(TextField).prop('value')).toEqual(simInputValue);
+      wrapper.find(Fab).simulate('click');
+      expect(onClickAdd).toHaveBeenCalled();
     });
   });
 });
